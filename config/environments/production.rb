1975acae915a9442aa2d6bde84f1c32d30fb5400
Justin Mullis <justin.mullis@gmail.com>
2016-01-03 17:37:56 -0500

    production mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,18 @@ Rails.application.configure do
       secret_access_key: Rails.application.secrets.aws_secret_access_key
     }
   }
+
+  # Mailer
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.delivery_method ||= :smtp
+  config.action_mailer.default_url_options = { :host => 'fast-inlet-8291.herokuapp.com' }
 end
